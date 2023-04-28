f1760426931eb7620dee12ce7357884f416a3e57
Vivek <vivek.rathod@walchandsangli.ac.in>
2023-04-29 02:44:28 +0530

    all api working correctly

--- LineApp/models.py
+++ LineApp/models.py
@@ -1,18 +1,137 @@
 from django.db import models
-
 # Create your models here.
 import json
 from datetime import datetime
-
-from django.contrib.auth.models import User
  
 class USER(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    joinedQueue = models.TextField(default='[]')
+    myQueue = models.TextField(default='[]')
     data = models.TextField(default='[]')
 
     def __str__(self):
         return self.username
+    
+    def getMyQueues(self):
+        try:
+            lst = json.loads(self.myQueue)
+            return lst
+        except:
+            print("cannot load myQueue")
+
+    def getJoinedQueues(self):
+        try:
+            lst = json.loads(self.joinedQueue)
+            return lst
+        except:
+            print("cannot load joinedQueue")
+
+    def joinQueue(self,creatorID1,queueName1):
+        try:
+            lst = json.loads(self.joinedQueue)
+            if(str(creatorID1)+"#"+str(queueName1) not in lst):
+                lst.append((str(creatorID1)+"#"+str(queueName1)))
+
+                if(Queue.objects.filter(queueName =queueName1,creatorID = creatorID1).exists()== False):
+                    print("queue doesn't exists")
+                else:            
+                    self.joinedQueue = json.dumps(lst)
+                    self.save()
+                    queryObj = Queue.objects.filter(queueName =queueName1,creatorID = creatorID1)
+                    queue = queryObj[0]
+                    queue.push(self.username)
+            else:
+                print("already joined queue")
+        except:
+            print("cannot join queue")
+
+    def exitQueue(self,creatorID1,queueName1):
+        try:
+            lst = json.loads(self.joinedQueue)
+            if(str(creatorID1)+"#"+str(queueName1) in lst):
+                lst.remove(str(creatorID1)+"#"+str(queueName1))
+                if(Queue.objects.filter(queueName =queueName1,creatorID = creatorID1).exists()== True):
+                    self.joinedQueue = json.dumps(lst)
+                    self.save()
+                    q = Queue.objects.filter(queueName =queueName1,creatorID = creatorID1)[0]
+                    q.removeByID(str(self.username))
+                else:
+                    print("queue does not exitst")
+            else:
+                print("cannto exit from queue as queue you specified does not exist")
+        except:
+            print("cannot exit from Queue")
+
+    def createQueue(self,queueName,location,time,note):
+        try:
+            lst = json.loads(self.myQueue)
+            if(str(self.username)+"#"+str(queueName) not in lst):
+                lst.append((str(self.username)+"#"+str(queueName)))
+
+                # creating new queue
+                newQueue = Queue.objects.create(queueName=queueName,creatorID=self.username,location=location,time=time,note=note)
+
+                self.myQueue = json.dumps(lst)
+                self.save()
+                newQueue.save()
+            else:
+                print("queue name already exists")
+        except:
+            print("cannot create queue")
+
+    def deleteQueue(self,queueName1):
+        try:
+            lst = json.loads(self.myQueue)
+            temp = str(self.username)+"#"+str(queueName1)
+
+            if(temp in lst):
+                lst.remove(temp)
+
+                if(Queue.objects.filter(queueName =queueName1,creatorID = self.username).exists()== True):
+                    q = Queue.objects.filter(queueName =queueName1,creatorID = self.username)[0]
+
+                    #### removing all customers from list ########
+                    customersList = json.loads(q.array)
+                    for customerID in customersList:
+                        thisUser = USER.objects.filter(username=customerID)[0]
+                        joinedList = json.loads(thisUser.joinedQueue)
+                        if temp in joinedList:
+                            joinedList.remove(temp)
+                            thisUser.joinedQueue = json.dumps(joinedList)
+                            thisUser.save()
+                    ##############################################
+                    q.delete()
+                    self.myQueue = json.dumps(lst)
+                    self.save()
+                    return 1
+                else:
+                    print("cannot delete as queue does not exitst")
+                    return -2
+            else:
+                print("can not delete queue as queue you specified does not exist")
+                return -1
+        except:
+            print("cannot delete Queue")
+            return 0
+
+    def pop(self, queueName1):
+        if(Queue.objects.filter(queueName=queueName1, creatorID=self.username).exists() == True):
+            q = Queue.objects.filter(queueName=queueName1, creatorID=self.username)[0]
+            popedPerson = q.pop()
+            return popedPerson
+        else:
+            print("cannot deletee queue as it is not in your created queue list")
+
+    def deletePersonFromQueue(self, queueName1, userID1):
+        if(Queue.objects.filter(queueName=queueName1, creatorID=self.username).exists() == True):
+            q = Queue.objects.filter(queueName=queueName1, creatorID=self.username)[0]
+            removedPerson = q.removeByID(userID1)
+            return removedPerson
+        else:
+            print("cannot deletee queue as it is not in your created queue list")
+
+
 
 class Queue(models.Model):
     queueName = models.CharField(max_length=20)
@@ -21,30 +140,39 @@ class Queue(models.Model):
     time = models.CharField(max_length=25)
     note = models.CharField(max_length=100)
     array = models.TextField(default='[]')
-    speedData = models.CharField(max_length=50, default="3000#2023-04-25 23:13:33")
+    speedData = models.CharField(max_length=50, default="300#2023-04-25 23:13:33")
 
     def __str__(self):
-        return self.queueName+"@"+self.creatorID
+        return self.creatorID+"@"+self.queueName
 
     def pop(self):
         if(self.array!="[]"):
             lst = json.loads(self.array)
             firstPerson = lst.pop(0)
             self.array = json.dumps(lst)
-            ####
+            self.save()
+
+            ##
+            if(USER.objects.filter(username=firstPerson).exists()==True):
+                u = USER.objects.filter(username=firstPerson)[0]
+                lst = json.loads(u.joinedQueue)
+                if(str(self.creatorID)+"#"+str(self.queueName) in lst):
+                    lst.pop(lst.index(str(self.creatorID)+"#"+str(self.queueName)))
+                    u.joinedQueue = json.dumps(lst)
+                    u.save()
+            ######### updating current speed #############
             speedData1 = self.speedData
 
-            past_time_string = speedData.split("#")[1]
-            stored_instance = datetime.strptime(past_time_string, '%Y-%m-%d %H:%M:%S')
+            stored_instance = datetime.strptime(speedData1.split("#")[1], '%Y-%m-%d %H:%M:%S')         # loading sotred time in instance
 
             now_instance = datetime.now()
-            now_time_string = now_instance.strftime('%Y-%m-%d %H:%M:%S')
+            now_time_string = now_instance.strftime('%Y-%m-%d %H:%M:%S')                             # loading now time in instance
 
-            time_difference_seconds = (now_instance - stored_instance).total_seconds()
+            time_difference_seconds = (now_instance - stored_instance).total_seconds()                  # finding difference in sec
 
-            self.speed = time_difference_seconds+"#"+now_time_string
+            self.speedData = str(time_difference_seconds)+"#"+now_time_string                           # updating speeddata
             ####
-            self.save()
+
             return firstPerson
         else:
             return None
@@ -61,7 +189,7 @@ class Queue(models.Model):
             firstPerson = lst[0]
             return firstPerson
         else:
-            return None
+            return 0
 
     def len(self):
         return len(json.loads(self.array))
@@ -75,8 +203,34 @@ class Queue(models.Model):
             except:
                 print("unable to get index of "+userID)
         else:
-            return None
+            return 0
 
     def getSpeed(self):
         speedData1 = self.speedData
         return speedData1.split("#")[0]
+
+    def removeByID(self,userID):
+        if(self.array!="[]"):
+            lst = json.loads(self.array)
+
+            try:
+                index = lst.index(userID)
+            except:
+                print("cannot delete as userID you specified does not exist in this qeueue")
+            
+            removedPerson = lst.pop(index)
+
+            if(USER.objects.filter(username=userID).exists()==True):
+                currentUser = USER.objects.filter(username=userID)[0]
+                userList = currentUser.getJoinedQueues()
+                if((str(self.creatorID)+"#"+str(self.queueName)) in userList):
+                    userList.remove(str(self.creatorID)+"#"+str(self.queueName))
+                    currentUser.joinedQueue = json.dumps(userList)
+                    currentUser.save()
+            
+            self.array = json.dumps(lst)
+            self.save()
+            return removedPerson
+        else:
+            print("cannot delete as queue is empty")
+            return 0
--- LineApp/views.py
+++ LineApp/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from LineApp.models import USER, Queue
 
 import jwt
+import json
 from LineApp.utils.auth import auth
 ########################################################################
 
@@ -20,10 +21,11 @@ def home(request):
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
-        userName = request.POST.get('userName')
-        password = request.POST.get('password')
-        print(userName)
-        print(password)
+        try:
+            userName = request.POST.get('userName')
+            password = request.POST.get('password')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
 
         queryObj = USER.objects.filter(username=userName, password=password)
 
@@ -57,7 +59,7 @@ def register(request):
     else:
         return JsonResponse({'status': 'check your method'})
 
-
+@csrf_exempt
 def protected(request):
     status = auth(request)
     if(status == -1):
@@ -66,3 +68,222 @@ def protected(request):
         return JsonResponse({'status': 'Invalid JWT token'})
     else:
         return JsonResponse({'status': "Authenticated !! your username is "+status})
+
+@csrf_exempt
+def joinqueue(request):
+    if request.method == 'POST':
+        try:
+            creatorid = request.POST.get('creatorID')
+            queuename = request.POST.get('queueName')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            USER.objects.filter(username=status)[0].joinQueue(creatorid,queuename)
+            return JsonResponse({'status': 'joined !'})
+
+@csrf_exempt
+def exitqueue(request):
+    if request.method == 'POST':
+        try:
+            creatorid = request.POST.get('creatorID')
+            queuename = request.POST.get('queueName')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            try:
+                USER.objects.filter(username=status)[0].exitQueue(creatorid, queuename)
+                return JsonResponse({'status': 'exited from queue !'})
+            except:
+                return JsonResponse({'status': 'unable to exit queue'})
+
+@csrf_exempt
+def myjoinedqueue(request):
+    status = auth(request)
+    if(status == -1):
+        return JsonResponse({'status': 'Invalid Authorization header'})
+    elif(status == 0):
+        return JsonResponse({'status': 'Invalid JWT token'})
+    else:
+        me = USER.objects.filter(username=status)[0]
+        joinedQueuesList = me.getJoinedQueues()
+        return JsonResponse({'status': joinedQueuesList})
+
+@csrf_exempt
+def createqueue(request):
+    if request.method == 'POST':
+        try:
+            queueName = request.POST.get('queueName')
+            location = request.POST.get('location')
+            time = request.POST.get('time')
+            note = request.POST.get('note')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            USER.objects.filter(username=status)[0].createQueue(queueName, location, time, note)
+            return JsonResponse({'status': 'created !'})
+
+@csrf_exempt
+def myqueue(request):
+    status = auth(request)
+    if(status == -1):
+        return JsonResponse({'status': 'Invalid Authorization header'})
+    elif(status == 0):
+        return JsonResponse({'status': 'Invalid JWT token'})
+    else:
+        me = USER.objects.filter(username=status)[0]
+        myQueueList = me.getMyQueues()
+        return JsonResponse({'status': myQueueList})
+
+@csrf_exempt
+def deletequeue(request):
+    if request.method == 'POST':
+        try:
+            queuename = request.POST.get('queueName')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            if(USER.objects.filter(username=status).exists()== True):
+
+                res = USER.objects.filter(username=status)[0].deleteQueue(queuename)
+                if res==0:
+                    return JsonResponse({'cannot delete Queue'})
+                elif res ==-1:
+                    return JsonResponse({'can not delete queue as queue you specified does not exist'})
+                elif res ==-2:
+                    return JsonResponse({'cannot delete as queue does not exitst'})
+                else:
+                    return JsonResponse({'status': 'queue deleted !'})
+            else:
+                return JsonResponse({'status': 'user doesnt exist'})
+
+@csrf_exempt
+def customerpanel(request):
+    if request.method == 'POST':
+        try:
+            creatorid = request.POST.get('creatorID')
+            queuename = request.POST.get('queueName')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            if(Queue.objects.filter(queueName=queuename, creatorID=creatorid).exists() == True):
+                q = Queue.objects.filter(queueName=queuename, creatorID=creatorid)[0]
+                index = q.indexof(status)
+
+                if(index==0):
+                    return JsonResponse({'status': "you cannot access as you are not in this queue"})
+                
+                speed = q.getSpeed()
+                data = {
+                    "length":q.len(),
+                    "first":q.first(),
+                    "myIndx":index,
+                    "speed":str(float(speed)/60.0)+" min/person",
+                    "estTime": str(((float(index)-1.0)*float(speed))/60.0)+" min"
+                }
+                return JsonResponse({'status': data})
+            else:
+                return JsonResponse({'status': "queue you specified does not exist"}) 
+
+@csrf_exempt
+def adminpanel(request):
+    if request.method == 'POST':
+        try:
+            queuename = request.POST.get('queueName')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            if(Queue.objects.filter(queueName=queuename, creatorID=status).exists() == True):
+                q = Queue.objects.filter(queueName=queuename, creatorID=status)[0]
+
+                if q.first()==0:
+                    first = "Empty Queue"
+                else:
+                    first = q.first()
+                
+                data = {
+                    "length":q.len(),
+                    "first":first,
+                    "speed":str(float(q.getSpeed())/60.0)+" min/person"
+                }
+                return JsonResponse({'status': data})
+            else:
+                return JsonResponse({'status': "queue you specified does not exist"})
+
+@csrf_exempt
+def pop(request):
+    if request.method == 'POST':
+        try:
+            queuename = request.POST.get('queueName')
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            if(USER.objects.filter(username=status).exists() == True):
+                u = USER.objects.filter(username=status)[0]
+                poppedPerson = u.pop(queuename)
+                return JsonResponse({'status': poppedPerson+" work done"})
+            else:
+                return JsonResponse({'status': 'user doesnt exist'})
+
+@csrf_exempt
+def deletepersonfromqueue(request):
+    if request.method == 'POST':
+        try:
+            queuename = request.POST.get('queueName')
+            userid = request.POST.get("userID")
+        except:
+            return JsonResponse({'status': 'no data in post req'})
+        
+        status = auth(request)
+        if(status == -1):
+            return JsonResponse({'status': 'Invalid Authorization header'})
+        elif(status == 0):
+            return JsonResponse({'status': 'Invalid JWT token'})
+        else:
+            if(USER.objects.filter(username=status).exists() == True):
+                u = USER.objects.filter(username=status)[0]
+                deletededPerson = u.deletePersonFromQueue(queuename, userid)
+                return JsonResponse({'status': deletededPerson+" deleted from queue"})
+            else:
+                return JsonResponse({'status': 'user doesnt exist'})
